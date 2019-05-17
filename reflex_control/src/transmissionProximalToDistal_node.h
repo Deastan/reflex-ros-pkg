4501961b3822ca326ba0257fdd165157f25b8eae
Jonathan <jonathanburkhard@gmail.com>
2019-05-17 17:04:20 +0200

    before add a mass to end effector

--- reflex_control/src/transmissionProximalToDistal_node.h
+++ reflex_control/src/transmissionProximalToDistal_node.h
@@ -2,38 +2,37 @@
  * Jonathan Burkhard, SMS-Lab, ETH Zurich, Switzerland
  * Jonathan Burkhard, CSEM S.A., Alpnach Dorf, Switzerland
  *
+ * Node Transmission link the proximal joint with the distal
+ *
  */
+
+ // Includes
 #include <string>
-#include <std_msgs/Bool.h>
+#include <boost/thread.hpp>
 
+// Function
 #include <std_msgs/Float64.h>
-// std_msgs/Float64.msg
-
-// ROS
-// #include <ros/ros.h>
-  
-#include "ros/ros.h"
 #include "std_msgs/String.h"
 
+// ROS
+#include "ros/ros.h"
 
+// Variables declaration
 
-
-// ROS part 
-// Initialization
-// std::string name_sensor; // Name of the sensors for the topic
-// ros::NodeHandle nh; /// node
-// ros::Publisher contact_publisher; // publisher for ros
-// std_msgs::Bool contact_message;
-
+//Publishers
 ros::Publisher publisher_proximal_1;
 ros::Publisher publisher_proximal_2;
 ros::Publisher publisher_proximal_3;
 
+// Messages
 std_msgs::Float64 contact_message_proximal_1;
 std_msgs::Float64 contact_message_proximal_2;
 std_msgs::Float64 contact_message_proximal_3;
 
-// void callback(const std_msgs::StringConstPtr& str);
+// Callback function
 void callback_proximal_1(const std_msgs::Float64& msg);
 void callback_proximal_2(const std_msgs::Float64& msg);
 void callback_proximal_3(const std_msgs::Float64& msg);
+
+// Non Lin. Function
+double function_nonLin(const std_msgs::Float64& msg);
--- reflex_gazebo/README.md
+++ reflex_gazebo/README.md
@@ -1,2 +1,4 @@
 Launch the robotic hand on Gazebo:
 $ roslaunch reflex_gazebo reflex_test.launch 
+
+Allow to run the gazebo simulation with the hand on the ground.
